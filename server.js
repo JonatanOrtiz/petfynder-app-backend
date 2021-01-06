c0a0d7f4b96141bdc2e68309f7b741a2ff1c8300
Jonatan Ortiz Maldonado <jonataneduard@gmail.com>
2021-01-06 09:42:37 -0300

    removed unecessary comments

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 const path = require("path");
 require("dotenv").config({ path: path.join(__dirname, './.env') });
-// const fs = require('fs');
-// const https = require('https');
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require("cors");
@@ -10,15 +8,6 @@ const app = express()
 const admin = require("firebase-admin");
 const { mongo } = require("./mongo.json");
 const serviceAccount = require("./petfynder-firebase-adminsdk-v2wpf-d0a2b0f3e7.json");
-// const certificate = fs.readFileSync(__dirname + '/../keyAndCertificate/publickey.cer', 'utf8');
-// const privateKey = fs.readFileSync(__dirname + '/../keyAndCertificate/privatekey.pem', 'utf8');
-// console.log(privateKey)
-// console.log(certificate)
-// const options = {
-//   key: privateKey,
-//   cert: certificate
-// };
-// const port = 443;
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -45,12 +34,5 @@ app.use("/files", express.static(path.resolve(__dirname, "..", "tmp", "uploads")
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "PetfynderFrontEnd/build", "index.html"))
 })
-// app.get('/', (req, res) => {
-//   res.send('<h1>Petfynder</h1>')
-// })
-// const server = https.createServer(options, app);
 
-// server.listen(port, () => {
-//   console.log("server starting on port : " + port)
-// });
 app.listen(5000)
